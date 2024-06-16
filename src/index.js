11ab61b91273f45ad1dfac71c6904f93afbd8ff5
balananujith <balananujith@gmail.com>
2024-06-16 18:32:27 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
